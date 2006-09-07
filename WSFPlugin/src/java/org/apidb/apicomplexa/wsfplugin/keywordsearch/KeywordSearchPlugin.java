11e6ec21a4b924cfccdf4b5522a7b10b8c5b295c
John Iodice <iodice@pennmedicine.upenn.edu>
2006-09-07 18:09:10 +0000

    initial check-in of web-service plugin for keyword search
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/trunk@13024 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/keywordsearch/KeywordSearchPlugin.java
+++ WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/keywordsearch/KeywordSearchPlugin.java
@@ -4,8 +4,10 @@
 package org.apidb.apicomplexa.wsfplugin.keywordsearch;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,28 +26,25 @@ import org.gusdb.wsf.plugin.WsfServiceException;
 public class KeywordSearchPlugin extends WsfPlugin {
 
     private class Match {
-
         public String geneID;
-        public String locations;
-        public int matchCount = 0;
-        public String sequence;
     }
+
+    private String scriptDir;
 
     private static final String PROPERTY_FILE = "keywordSearch-config.xml";
 
     // required parameter definition
-    public static final String PARAM_DATASET = "Dataset";
-    public static final String PARAM_EXPRESSION = "Expression";
+    public static final String PARAM_KEYWORD_EXPRESSION = "keyword_expression";
+    public static final String PARAM_CASE_INDEPENDENT = "case_independent";
+    public static final String PARAM_DATASETS = "datasets";
+    public static final String PARAM_MAX_PVALUE = "max_pvalue";
+    public static final String PARAM_SPECIES_NAME = "species_name";
 
     public static final String COLUMN_GENE_ID = "GeneID";
-    public static final String COLUMN_LOCATIONS = "Locations";
-    public static final String COLUMN_MATCH_COUNT = "MatchCount";
-    public static final String COLUMN_SEQUENCE = "Sequence";
 
     // field definition
     private static final String FIELD_DATA_DIR = "DataDir";
-    private static final String FIELD_SOURCEID_REGEX = "SourceIdRegex";
-    private static final String FIELD_MAX_LENGTH = "MaxLength";
+    private static final String FIELD_SCRIPT_DIR = "ScriptDir";
 
     private File dataDir;
     private String sourceIdRegex;
@@ -56,21 +55,17 @@ public class KeywordSearchPlugin extends WsfPlugin {
      * 
      */
     public KeywordSearchPlugin() throws WsfServiceException {
-        super(PROPERTY_FILE);
-        // load properties
-        String dir = getProperty(FIELD_DATA_DIR);
-        if (dir == null)
-            throw new WsfServiceException(
-                    "The required field in property file is missing: "
-                            + FIELD_DATA_DIR);
+	super(PROPERTY_FILE);
+	// load properties
+	String dir = getProperty(FIELD_DATA_DIR);
+	if (dir == null)
+	    throw new WsfServiceException(
+					  "The required field in property file is missing: "
+					  + FIELD_DATA_DIR);
         dataDir = new File(dir);
         logger.info("constructor(): dataDir: " + dataDir.getName() + "\n");
 
-        sourceIdRegex = getProperty(FIELD_SOURCEID_REGEX);
-
-        String maxLength = getProperty(FIELD_MAX_LENGTH);
-        if (maxLength != null) maxLen = Integer.parseInt(maxLength);
-        else maxLen = 4000; // default value
+	scriptDir = getProperty(FIELD_SCRIPT_DIR);
     }
 
     /*
@@ -80,8 +75,9 @@ public class KeywordSearchPlugin extends WsfPlugin {
      */
     @Override
     protected String[] getRequiredParameterNames() {
-        return new String[] { PARAM_EXPRESSION }; // , PARAM_DATASET };
+        return new String[] { PARAM_KEYWORD_EXPRESSION, PARAM_DATASETS, PARAM_SPECIES_NAME };
     }
+
 
     /*
      * (non-Javadoc)
@@ -103,19 +99,6 @@ public class KeywordSearchPlugin extends WsfPlugin {
             throws WsfServiceException {
         // do nothing in this plugin
 
-        boolean datasetPresent = false;
-        for (String param : params.keySet()) {
-            logger.debug("Param - name=" + param + ", value="
-                    + params.get(param));
-            if (param.startsWith(PARAM_DATASET)) {
-                datasetPresent = true;
-                break;
-            }
-        }
-        if (!datasetPresent)
-            throw new WsfServiceException(
-                    "The required dataset parameter is not presented.");
-
     }
 
     /*
@@ -129,220 +112,66 @@ public class KeywordSearchPlugin extends WsfPlugin {
         logger.info("Invoking KeywordSearchPlugin...");
 
         // get parameters
-        // String datasetIDs = params.get(PARAM_DATASET);
-        String datasetIDs = null;
-        for (String paramName : params.keySet()) {
-            if (paramName.startsWith(PARAM_DATASET)) {
-                datasetIDs = params.get(paramName);
-                break;
-            }
-        }
+        String datasets = params.get(PARAM_DATASETS);
+        String keywordExpression = params.get(PARAM_KEYWORD_EXPRESSION);
+        String caseIndependent = params.get(PARAM_CASE_INDEPENDENT);
+	String maxPvalue = params.get(PARAM_MAX_PVALUE);
+	String species_name = params.get(PARAM_SPECIES_NAME);
 
-	// for debugging
-	datasetIDs = "";
+	StringBuffer cmd = new StringBuffer("(");
 
-        String expression = params.get(PARAM_EXPRESSION);
+	// iterate through datasets
+	String[] ds = datasets.split(",");
+	for (String dataset : ds) {
+	    cmd.append(scriptDir + "/filterByValue -n " + maxPvalue + " < " + dataDir
+		       + "/" + dataset + " | " + scriptDir + "/filterByValue -s '"
+		       + species_name + "' | grep " + caseIndependent + " '" + keywordExpression
+		       + "';");
+	}
 
-        // translate the expression
-        String regex = translateExpression(expression);
+       	cmd.append(") | cut -f1 | sort -u");
+	//	cmd.append(")");
 
-        // open the flatfile database assigned by the user 
+	logger.info("\ncommand line = \"" + cmd + "\"\n\n");
 
-	/*
-	  try { */
-            List<Match> matches = new ArrayList<Match>();
-	    /*            String[] dsIds = datasetIDs.split(",");
+	// make it a string array to fool exec() into working
+	//	String[] cmds = new String[] {cmd.toString(), "|",  "cut", "-f1",  "|", "sort", "-u"};
+	String[] cmds = new String[] {"bash", "-c", cmd.toString()};
 
-            // scan on each dataset, and add matched keywords in the result
-            for (String dsId : dsIds) {
-                logger.info("execute(): dsId: " + dsId + "\n");
+	// run it
+	try {
+            String output = invokeCommand(cmds, 10 * 60);
+	    System.out.println("output is " + output);
+	    logger.info("output is " + output);
+	    //            long end = System.currentTimeMillis();
+	    //            logger.info("Invocation takes: " + ((end - start) / 1000.0)
+	    //                    + " seconds");
 
-            }
-	*/
+            if (exitValue != 0)
+                throw new WsfServiceException("The invocation is failed: "
+                        + output);
 
-            // construct results
-            return prepareDummyResult(matches, orderedColumns);
-	    /*        } catch (IOException ex) {
+	    List<Match> matches = new ArrayList<Match>();
+
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(output.getBytes())));
+	    String nameIn;
+	    while ((nameIn = reader.readLine()) != null) {
+
+		logger.info("match GeneID: " + nameIn);
+
+		Match match = new Match();
+		match.geneID = nameIn;
+		matches.add(match);
+	    }
+
+	    // construct results
+            return prepareResult(matches, orderedColumns);
+	} catch (Exception ex) {
             throw new WsfServiceException(ex);
         }
-	    */
+
     }
 
-    /**
-     * 
-     * @param fileID
-     * @return
-     * @throws IOException
-     */
-    private File openDataFile(String datasetID) throws IOException {
-        logger.info("openDataFile(): dataDir: " + dataDir.getName()
-                + ", datasetID: " + datasetID + "\n");
-
-        File dataFile = new File(dataDir, datasetID);
-        if (!dataFile.exists()) throw new IOException("The dataset \""
-                + datasetID + "\" cannot be found.");
-        else return dataFile;
-    }
-
-    private String translateExpression(String expression) {
-        // remove spaces
-        String regex = expression.replaceAll("\\s", "");
-
-        // replace '(' to '{', ')' to '}', 'x' to '.', '<' to '(', '>' to ')'
-        regex = regex.replace('(', '{');
-        regex = regex.replace(')', '}');
-        regex = regex.replace('x', '.');
-        regex = regex.replace('<', '(');
-        regex = regex.replace('>', ')');
-
-        // remove '-'
-        regex = regex.replaceAll("[\\-]", "");
-
-        // replace numbers/number pairs by surrounding them with "{}"
-        Pattern pattern = Pattern.compile("\\(\\d+(,\\d+)?\\)");
-        Matcher matcher = pattern.matcher(regex);
-        int prev = 0;
-        StringBuffer sb = new StringBuffer();
-        while (matcher.find()) {
-            // get previous part
-            sb.append(regex.substring(prev, matcher.start()));
-            // replace '(' to '{'
-            sb.append('{');
-            sb.append(regex.subSequence(matcher.start() + 1, matcher.end() - 1));
-            sb.append('}');
-            prev = matcher.end();
-        }
-        // add last part into the buffer
-        sb.append(regex.substring(prev));
-        regex = sb.toString();
-
-        // replace {string} into ignoring characters
-        pattern = Pattern.compile("\\{(\\D+?)\\}");
-        matcher = pattern.matcher(regex);
-        prev = 0;
-        sb.delete(0, sb.length());
-        while (matcher.find()) {
-            // get the previous part
-            sb.append(regex.substring(prev, matcher.start()));
-            // replace '{' with "[^"
-            sb.append("[^");
-            sb.append(regex.substring(matcher.start() + 1, matcher.end() - 1));
-            sb.append(']');
-            prev = matcher.end();
-        }
-        // add the last part
-        sb.append(regex.substring(prev));
-        regex = sb.toString();
-
-        // Split the string by '{.*}'
-        String splitStub = "SplitStub";
-        regex = regex.replaceAll("\\{", splitStub + "{");
-        regex = regex.replaceAll("\\}", "}" + splitStub);
-        String[] parts = regex.split(splitStub);
-        sb.delete(0, sb.length());
-        for (String part : parts) {
-            // check if it contains '{'
-            if (part.indexOf('{') < 0) { // not containing '{.*}'
-                // replace amino acids shortcuts
-            }
-            sb.append(part);
-        }
-        return sb.toString();
-    }
-
-    private List<Match> findMatches(String datasetID, String regex,
-            String colorCode, int contextLength) throws IOException,
-            WsfServiceException {
-        File datasetFile = openDataFile(datasetID);
-        BufferedReader in = new BufferedReader(new FileReader(datasetFile));
-
-        List<Match> matches = new ArrayList<Match>();
-        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-
-        // read header of the first sequence
-        String line;
-        line = in.readLine();
-        while (true) {
-            // get first gene id
-            line = line.trim().replaceAll("\\s", " ");
-            String geneID = extractField(line, sourceIdRegex);
-
-            StringBuffer seq = new StringBuffer();
-            while ((line = in.readLine()) != null) {
-                line = line.trim();
-                if (line.length() == 0) continue;
-                // check if we reach the header of the next sequence
-                // if so, we just finishe reading the sequence, exit inner loop
-                if (line.charAt(0) == '>') break;
-                seq.append(line);
-            }
-            // check if we reach the end of the file
-            if (line == null) break;
-
-            // scan the sequence to find all matched locations
-            Match match = findLocations(geneID, pattern, seq.toString(),
-                    colorCode, contextLength);
-            if (match != null) matches.add(match);
-        }
-        return matches;
-    }
-
-    private Match findLocations(String geneID, Pattern pattern,
-            String sequence, String colorCode, int contextLength)
-            throws WsfServiceException {
-        Match match = new Match();
-        match.geneID = geneID;
-        StringBuffer sbLoc = new StringBuffer();
-        StringBuffer sbSeq = new StringBuffer();
-        int prev = 0;
-
-        Matcher matcher = pattern.matcher(sequence);
-        while (matcher.find()) {
-            // add locations
-            if (sbLoc.length() != 0) sbLoc.append(",");
-            sbLoc.append('(');
-            sbLoc.append(matcher.start());
-            sbLoc.append("-");
-            sbLoc.append(matcher.end() - 1);
-            sbLoc.append(')');
-
-            // abtain the context sequence
-            if ((matcher.start() - prev) <= (contextLength * 2)) {
-                // no need to trim
-                sbSeq.append(sequence.substring(prev, matcher.start()));
-            } else { // need to trim some
-                if (prev != 0)
-                    sbSeq.append(sequence.substring(prev, prev + contextLength));
-                sbSeq.append("...");
-                sbSeq.append(sequence.substring(
-                        matcher.start() - contextLength, matcher.start()));
-            }
-            sbSeq.append("<font color=\"" + colorCode + "\">");
-            sbSeq.append(sequence.substring(matcher.start(), matcher.end()));
-            sbSeq.append("</font>");
-            prev = matcher.end();
-            match.matchCount++;
-        }
-        if (match.matchCount == 0) return null;
-
-        // grab the last context
-        if ((prev + contextLength) < sequence.length()) {
-            sbSeq.append(sequence.substring(prev, prev + contextLength));
-            sbSeq.append("...");
-        } else {
-            sbSeq.append(sequence.substring(prev));
-        }
-        // check if the match is too unspecific, that is, the length is bigger
-        // than 400 characters (the maximum length of varchar in Oracle); if so,
-        // throw an exception to state that
-        if (sbSeq.length() > maxLen || sbLoc.length() > maxLen)
-            throw new WsfServiceException("The expression hits too many "
-                    + "locations than the system can handle. Please make it "
-                    + "more specific.");
-        match.locations = sbLoc.toString();
-        match.sequence = sbSeq.toString();
-        return match;
-    }
 
     private String[][] prepareResult(List<Match> matches, String[] cols) {
         String[][] result = new String[matches.size()][cols.length];
@@ -355,9 +184,6 @@ public class KeywordSearchPlugin extends WsfPlugin {
             Match match = matches.get(i);
 
             result[i][orders.get(COLUMN_GENE_ID)] = match.geneID;
-            result[i][orders.get(COLUMN_LOCATIONS)] = match.locations;
-            result[i][orders.get(COLUMN_MATCH_COUNT)] = Integer.toString(match.matchCount);
-            result[i][orders.get(COLUMN_SEQUENCE)] = match.sequence;
         }
         return result;
     }
