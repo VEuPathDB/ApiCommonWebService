363bf17a0a4e5e64e363c65f5ba8dfe7462c1bc3
Cary Pennington <carypen@_.com>
2007-01-09 14:06:55 +0000

    updated version of this plugin to fix several issues
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/trunk@14757 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/apifed/ApiFedPlugin.java
+++ WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/apifed/ApiFedPlugin.java
@@ -24,20 +24,33 @@ import org.gusdb.wsf.client.WsfServiceServiceLocator;
  */
 public class ApiFedPlugin extends WsfPlugin {
 	
-    //Static Values
-    public static final String CRYPTO_WSF_URL = "http://mango.ctegd.uga.edu:8081/axis/services/WsfService";
-    public static final String PLASMO__WSF_URL = "http://qa.plasmodb.org/plasmoaxis/";
-    public static final String TOXO_WSF_URL = "http://qa.plasmodb.org/plasmoaxis/";
+    //Static Property Values
+    public static final String PROPERTY_FILE = "apifed-config.xml";
+    public static final String CRYPTO_WSF_URL = "CryptoURL";
+    public static final String PLASMO_WSF_URL = "PlasmoURL";
+    public static final String TOXO_WSF_URL = "ToxoURL";
+
     //Input Parameters
     public static final String PARAM_PROCESSNAME = "ProcessName";
     public static final String PARAM_PARAMETERS = "Parameters";
     public static final String PARAM_COLUMNS = "Columns";
+    public static final String PARAM_ORGANISMS = "Organism";
     public static final String PARAM_QUERY = "Query";
     
     //Output Parameters
     public static final String COLUMN_RETURN = "Response";
     
-    public ApiFedPlugin() throws WsfServiceException {}
+    //Member Variable
+    private String cryptoUrl;
+    private String plasmoUrl;
+    private String toxoUrl;
+
+    public ApiFedPlugin() throws WsfServiceException {
+	super(PROPERTY_FILE);
+	cryptoUrl = getProperty(CRYPTO_WSF_URL);
+	plasmoUrl = getProperty(PLASMO_WSF_URL);
+	toxoUrl = getProperty(TOXO_WSF_URL);
+    }
 
     /*
      * (non-Javadoc)
@@ -84,10 +97,19 @@ public class ApiFedPlugin extends WsfPlugin {
     @Override
     protected String[][] execute(String queryName, Map<String, String> params,
             String[] orderedColumns) throws WsfServiceException {
-	    
-	    String orgsString = params.get(PARAM_ORGANISMS);
-	    String[] calls = getRemoteCalls(orgsString);
-
+	
+	    String[] calls = {"doCrypto","",""};
+	/*
+	    if(params.containsKey(PARAM_ORGANISMS)){
+		String orgsString = params.get(PARAM_ORGANISMS);
+		calls = getRemoteCalls(orgsString);
+	    } else{
+		calls[0] = "doCrypto";
+	        calls[1] = "doPlasmo";
+	        calls[2] = "doToxo";
+	    }
+	*/
+	
 	    String query = "";	    
 	    String processName = "org.apidb.apicomplexa.wsfplugin.wdkquery.WdkQueryPlugin";
 	    if(params.containsKey(PARAM_QUERY)){
@@ -96,40 +118,59 @@ public class ApiFedPlugin extends WsfPlugin {
 	    } else{query = queryName;}
 	       logger.info("QueryName = "+ query);
 	    String[] arrayParams = getParams(params);
-	    
+	    String[] componentColumns = removeProjectId(orderedColumns);
+
 	    //String url = "http://mango.ctegd.uga.edu:8081/axis/services/WsfService";
-	    String url = "http://localhost:8082/axis/services/WsfService";
-	    //String url = "http://mango.ctegd.uga.edu:9020/axis/services/WsfService";
-	    //String url = "http://qa.plasmodb.org/plasmoaxis/axis/services/WsfService";
-	   
+	    //String url = "http://localhost:8082/axis/services/WsfService";
+	    
+	    //String cryptoUrl = "http://mango.ctegd.uga.edu:9020/axis/services/WsfService";
+	    //String plasmoUrl = "http://qa.plasmodb.org/plasmoaxis/axis/services/WsfService";
+	    //String toxoUrl = "http://qa.plasmodb.org/plasmoaxis/axis/services/WsfService";
+
+	    //Objects to hold the results of the threads
 	    CompResult cryptoResult = new CompResult();
-	    CompResult cryptoResult2 = new CompResult();
-	    //String[][] toxoResult = null;
-	    Status threadStatus = new Status(false);
-	    Status threadStatus2 = new Status(false);
-	    Thread myThread = new WdkQuery(url, processName, queryName, arrayParams, orderedColumns, cryptoResult, threadStatus); 
-	    Thread myThread2 = new WdkQuery(url, processName, queryName, arrayParams, orderedColumns, cryptoResult2, threadStatus2);
-	    myThread.start();
-	    myThread2.start();
-	    while(!(threadStatus.getDone() && threadStatus2.getDone())){
+	    CompResult plasmoResult = new CompResult();
+	    CompResult toxoResult = new CompResult();
+	    
+	    //Objects to hold the status of the Threads
+	    Status cryptoThreadStatus = new Status(false);
+	    Status plasmoThreadStatus = new Status(false);
+	    Status toxoThreadStatus = new Status(false);
+	   
+	    //Call only the needed component sites
+	    if(calls[0].equals("")){cryptoThreadStatus.setDone(true);}
+	    else {
+		Thread cryptoThread = 
+		    new WdkQuery(cryptoUrl, processName, queryName, arrayParams, componentColumns, cryptoResult, cryptoThreadStatus);
+		cryptoThread.start();
+	    }
+	    if(calls[1].equals("")){plasmoThreadStatus.setDone(true);}
+	    else {
+		Thread plasmoThread = 
+		    new WdkQuery(plasmoUrl, processName, queryName, arrayParams, componentColumns, plasmoResult, plasmoThreadStatus);
+		plasmoThread.start();
+	    }
+	    if(calls[2].equals("")){toxoThreadStatus.setDone(true);}
+	    else {
+		Thread toxoThread = 
+		    new WdkQuery(toxoUrl, processName, queryName, arrayParams, componentColumns, toxoResult, toxoThreadStatus);
+		toxoThread.start();
+	    }
+	   
+	    while(!(cryptoThreadStatus.getDone() && plasmoThreadStatus.getDone() && toxoThreadStatus.getDone())){
 		try{
-		    logger.info(threadStatus.getDone());
+		    logger.info(cryptoThreadStatus.getDone());
 		    Thread.sleep(1000);
 		continue;
 		}catch(InterruptedException e){}
 	    }
+	    
+
 	    String[][] result = null;
-	    result = cryptoResult.getAnswer();
+	    result = combineResults(cryptoResult.getAnswer(), plasmoResult.getAnswer(), toxoResult.getAnswer(), orderedColumns);
+	    //result = cryptoResult.getAnswer();
 	    
-	    String[][] responseT = new String[result.length][orderedColumns.length];
-	    
-	    for(int i = 0; i < result.length; i++){
-	    	for(int j = 0; j < orderedColumns.length; j++){
-	    	     responseT[i][j] = result[i][j];
-	    	}
-	    }
-	    
-	    return responseT; 
+	    return result; 
     }
     
     private String[] getParams (Map<String,String> params)
@@ -148,9 +189,9 @@ public class ApiFedPlugin extends WsfPlugin {
     
     private String[] getRemoteCalls(String orgs)
     {
-	String[] calls = new String[3];
+	String[] calls = {"","",""};
 	String[] orgArray = orgs.split(",");
-	for(organism:orgArray){
+	for(String organism:orgArray){
 	    if(organism.charAt(0)=='C')
 		calls[0] = "doCrypto";
 	    if(organism.charAt(0)=='P')
@@ -158,12 +199,122 @@ public class ApiFedPlugin extends WsfPlugin {
 	    if(organism.charAt(0)=='T')
 		calls[2] = "doToxo";
 	}
+	return calls;
+    }
+
+    private String[][] combineResults(String[][] crypto, String[][] plasmo, String[][] toxo, String[] cols)
+    {
+	//Determine the length of the new array
+	int numrows = 0;
+	if(crypto!=null)numrows = numrows + crypto.length;
+	if(plasmo!=null)numrows = numrows + plasmo.length;
+	if(toxo!=null)numrows = numrows + toxo.length;
+	int i = 0;
+	String[][] combined = new String[numrows][cols.length + 1]; //add one column for the addition of projectId
+
+	//Find the index for the projectId columns
+	int projectIndex = 0;
+	for(String col:cols){
+	    if(col.equals("project_id")){break;}
+	    else{projectIndex++;}
+	}
+
+	//Add crypto result, if it is not empty, to the final results
+	if(crypto!=null){
+	    if(!crypto[0][0].equals("ERROR")){
+		for(String[] rec:crypto){
+		    logResults(rec);
+		    combined[i] = rec;
+		    combined[i] = insertProjectId(combined[i], projectIndex, "cryptodb");
+		    i++;
+		}
+	    }//else {TODO :: Process this error to display message in the header section of the Summary Page	
+	}
+
+	//Add plasmo result, if it is not empty, to the final results
+	if(plasmo!=null){
+	    if(!plasmo[0][0].equals("ERROR")){
+		for(String[] rec:plasmo){
+		    combined[i] = rec;
+		    combined[i] = insertProjectId(combined[i], projectIndex, "plasmodb");
+		    i++;
+		}
+	    }//else {TODO :: Process this error to display message in the header section of the Summary Page	
+	}
+	
+	//Add toxo result, if it is not empty, to the final results
+	if(toxo!=null){
+	    if(!toxo[0][0].equals("ERROR")){
+		for(String[] rec:toxo){
+		    combined[i] = rec;
+		    combined[i] = insertProjectId(combined[i], projectIndex, "toxodb");
+		    i++;
+		}
+	    }//else {TODO :: Process this error to display message in the header section of the Summary Page
+	}
+	return combined;
+    }
+    
+    private void logResults(String[] r){
+	for(String cr:r){
+	    logger.info("------- Record : " + cr);
+	}
+    }
+
+    // Method to insert the projectId in the proper place in the return data
+    private String[] insertProjectId(String[] rec, int index, String projectId)
+    {
+	String[] newRec = new String[rec.length+1];
+	if(index!=rec.length){
+	    String t1 = "";
+	    int newi = 0;
+	    for(int i = 0; i<rec.length; i++){
+		if(i==index){
+		    t1 = rec[i];
+		    newRec[newi] = projectId;
+		    newi++;
+		    newRec[newi] = t1;
+		}else{
+		    newRec[newi] = rec[i];
+		}
+		newi++;
+	    }
+	}else{
+	    for(int i = 0;i<rec.length;i++){
+		newRec[i] = rec[i];
+	    }
+	    newRec[newRec.length-1] = projectId;
+	}
+	return newRec;
+    }
+
+    // Method to remove the projectId column from the colums sent to the component sites
+    private String[] removeProjectId(String[] cols)
+    {
+	int projectIndex = 0;
+	for(String col:cols){
+	    logger.info("------------"+col+"---"+col.equals("project_id")+"-----------");
+	    if(col.equals("project_id")){break;}
+	    else{projectIndex++;}
+	}
+	int newindex = 0;
+	String[] newCols = new String[cols.length - 1];
+	for(int i = 0;i<cols.length;i++){
+	    if(i==projectIndex){
+		continue;
+	    }
+	    newCols[newindex] = cols[i];
+	    newindex++;
+	}
+	return newCols;
     }
 
     //Inner Class to do invokations
     class CompResult {
 	private String[][] answer;
-	public CompResult(){}
+	public CompResult(){
+	    answer = null;
+	}
 	public void setAnswer(String[][] answer){
 	    this.answer = answer;
 	}
