8a030b42d76f62e39aebf615d88b6472157ca400
Xin Gao <xingao@upenn.edu>
2010-05-04 21:41:57 +0000

    correct a typo in the package name. finish the span operation code.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/branches/spanlogic@34827 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/spanlogic/SpanCompositionPlugin.java
+++ WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/spanlogic/SpanCompositionPlugin.java
@@ -1,18 +1,21 @@
 package org.apidb.apicomplexa.wsfplugin.spanlogic;
 
 import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 import org.gusdb.wdk.controller.CConstants;
-import org.gusdb.wdk.controller.action.ActionUtility;
 import org.gusdb.wdk.model.AnswerValue;
 import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
-import org.gusdb.wdk.model.jspwrap.UserBean;
+import org.gusdb.wdk.model.dbms.SqlUtils;
 import org.gusdb.wdk.model.jspwrap.WdkModelBean;
-import org.gusdb.wdk.model.user.AnswerFactory;
 import org.gusdb.wdk.model.user.Step;
 import org.gusdb.wdk.model.user.User;
 import org.gusdb.wsf.plugin.WsfPlugin;
@@ -85,9 +88,10 @@ public class SpanCompositionPlugin extends WsfPlugin {
             String cacheA = getCache(wdkModel, userSignature, params, "a");
             String cacheB = getCache(wdkModel, userSignature, params, "b");
 
-            String sql = composeSql(projectId, operation, cacheA, cacheB, startStopA,
-                    startStopB, output);
-            return getResult(wdkModel, sql);
+            String sql = composeSql(projectId, operation, cacheA, cacheB,
+                    startStopA, startStopB, output);
+
+            return getResult(wdkModel, sql, orderedColumns);
         } catch (Exception ex) {
             throw new WsfServiceException(ex);
         }
@@ -114,62 +118,117 @@ public class SpanCompositionPlugin extends WsfPlugin {
         if (begin == null) begin = PARAM_VALUE_START;
         String end = params.get(PARAM_SPAN_END + suffix);
         if (end == null) end = PARAM_VALUE_STOP;
-        String beginDirection = params.get(PARAM_SPAN_BEGIN_DIRECTION + suffix);
-        if (beginDirection == null) beginDirection = PARAM_VALUE_UPSTREAM;
-        String endDirection = params.get(PARAM_SPAN_END_DIRECTION + suffix);
-        if (endDirection == null) endDirection = PARAM_VALUE_UPSTREAM;
-        int beginOffset = 0;
-        if (params.containsKey(PARAM_SPAN_BEGIN_OFFSET + suffix))
-            beginOffset = Integer.valueOf(params.get(PARAM_SPAN_BEGIN_OFFSET
-                    + suffix));
-        int endOffset = 0;
-        if (params.containsKey(PARAM_SPAN_END_OFFSET + suffix))
-            endOffset = Integer.valueOf(params.get(PARAM_SPAN_END_OFFSET
-                    + suffix));
 
-        beginOffset *= beginDirection.equals(PARAM_VALUE_UPSTREAM) ? -1 : 1;
-        endOffset *= endDirection.equals(PARAM_VALUE_UPSTREAM) ? -1 : 1;
+        String beginDir = params.get(PARAM_SPAN_BEGIN_DIRECTION + suffix);
+        if (beginDir == null) beginDir = PARAM_VALUE_UPSTREAM;
+        String endDir = params.get(PARAM_SPAN_END_DIRECTION + suffix);
+        if (endDir == null) endDir = PARAM_VALUE_UPSTREAM;
+
+        int beginOff = 0;
+        if (params.containsKey(PARAM_SPAN_BEGIN_OFFSET + suffix))
+            beginOff = Integer.valueOf(params.get(PARAM_SPAN_BEGIN_OFFSET
+                    + suffix));
+        int endOff = 0;
+        if (params.containsKey(PARAM_SPAN_END_OFFSET + suffix))
+            endOff = Integer.valueOf(params.get(PARAM_SPAN_END_OFFSET + suffix));
+
+        if (beginDir.equals(PARAM_VALUE_UPSTREAM)) beginOff *= -1;
+        if (endDir.equals(PARAM_VALUE_UPSTREAM)) endOff *= -1;
 
         String table = "fl" + suffix + ".";
-        // (CASE fla.IS_REVERSED WHEN 0 THEN fla.start_min - $$start_off$$ ELSE
-        // fla.end_max + $$end_off$$ END)
+
         StringBuilder builder = new StringBuilder("(CASE ");
-        builder.append(table + "is_reversed WHEN 0 THEN ");
-        builder.append(table + begin);
-        builder.append(((beginOffset >= 0) ? " + " : " ") + beginOffset);
-        builder.append(" ELSE " + table + end);
-        builder.append(((endOffset <= 0) ? " + " : " ") + (-endOffset));
-        builder.append(")");
+        builder.append("WHEN " + table + "is_reversed = 0 ");
+        builder.append(" AND '" + begin + "' = 'start' ");
+        builder.append("THEN (" + table + "start_min + 1*(" + beginOff + ")) ");
+        builder.append("WHEN " + table + "is_reversed = 0 ");
+        builder.append(" AND '" + begin + "' = 'stop' ");
+        builder.append("THEN (" + table + "end_max + 1*(" + beginOff + ")) ");
+        builder.append("WHEN " + table + "is_reversed = 1 ");
+        builder.append(" AND '" + end + "' = 'start' ");
+        builder.append("THEN (" + table + "end_max - 1*(" + endOff + ")) ");
+        builder.append("ELSE (" + table + "start_min - 1*(" + endOff + ")) ");
+        builder.append("END)");
         String start = builder.toString();
 
         // (CASE fla.IS_REVERSED WHEN 0 THEN fla.end_max - 80 ELSE fla.start_min
         // + 50 END)
         builder = new StringBuilder("(CASE ");
-        builder.append(table + "is_reversed WHEN 0 THEN ");
-        builder.append(table + end);
-        builder.append(((endOffset >= 0) ? " + " : " ") + endOffset);
-        builder.append(" ELSE " + table + start);
-        builder.append(((beginOffset <= 0) ? " + " : " ") + (-beginOffset));
-        builder.append(")");
-
+        builder.append("WHEN " + table + "is_reversed = 0 ");
+        builder.append(" AND '" + end + "' = 'start' ");
+        builder.append("THEN (" + table + "start_min + 1*(" + endOff + ")) ");
+        builder.append("WHEN " + table + "is_reversed = 0 ");
+        builder.append(" AND '" + end + "' = 'stop' ");
+        builder.append("THEN (" + table + "end_max + 1*(" + endOff + ")) ");
+        builder.append("WHEN " + table + "is_reversed = 1 ");
+        builder.append(" AND '" + begin + "' = 'start' ");
+        builder.append("THEN (" + table + "end_max - 1*(" + beginOff + ")) ");
+        builder.append("ELSE (" + table + "start_min - 1*(" + beginOff + ")) ");
+        builder.append("END)");
         String stop = builder.toString();
+
         return new String[] { start, stop };
     }
 
-    private String composeSql(String projectId, String operation, String sqlA, String sqlB,
-            String[] regionA, String[] regionB, String output) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("SELECT fl" + output + ".feature_source_id AS source_id");
-        
-        
-        
-        
+    private String composeSql(String projectId, String operation, String sqlA,
+            String sqlB, String[] regionA, String[] regionB, String output) {
+        StringBuilder builder = new StringBuilder("SELECT ");
+        builder.append(" f" + output + ".* FROM ");
+        builder.append("(SELECT fla.feature_source_id AS source_id, ");
+        builder.append("        ca.project_id, ca.wdk_weight, ");
+        builder.append(regionA[0] + " AS begin_a, " + regionA[1] + " AS end_a");
+        builder.append(" FROM apidb.FEATURELOCATION fla, " + sqlA + " ca ");
+        builder.append(" WHERE fla.feature_source_id = ca.source_id ");
+        builder.append("   AND fla.is_top_level = 1) fa, ");
+        builder.append("(SELECT flb.feature_source_id AS source_id, ");
+        builder.append("        cb.project_id, cb.wdk_weight, ");
+        builder.append(regionA[0] + " AS begin_b, " + regionA[1] + " AS end_b");
+        builder.append(" FROM apidb.FEATURELOCATION flb, " + sqlB + " cb ");
+        builder.append(" WHERE flb.feature_source_id = cb.source_id ");
+        builder.append("   AND flb.is_top_level = 1) fb ");
+
+        if (operation.equals(PARAM_VALUE_OVERLAP)) {
+            builder.append("WHERE fa.begin_a <= fb.end_b ");
+            builder.append("  AND fa.end_a >= fb.begin_b ");
+        } else if (operation.equals(PARAM_VALUE_A_CONTAIN_B)) {
+            builder.append("WHERE fa.begin_a <= fb.begin_b ");
+            builder.append("  AND fa.end_a >= fb.end_b ");
+        } else { // b_contain_a
+            builder.append("WHERE fa.begin_a >= fb.begin_b ");
+            builder.append("  AND fa.end_a <= fb.end_b ");
+        }
+
         return builder.toString();
     }
 
-    private WsfResult getResult(WdkModel wdkModel, String sql) {
+    private WsfResult getResult(WdkModel wdkModel, String sql,
+            String[] orderedColumns) throws WdkUserException,
+            WdkModelException, SQLException {
+        DataSource dataSource = wdkModel.getQueryPlatform().getDataSource();
+        ResultSet results = null;
+        try {
+            results = SqlUtils.executeQuery(wdkModel, dataSource, sql);
+            List<String[]> records = new ArrayList<String[]>();
+            while (results.next()) {
+                String[] record = new String[orderedColumns.length];
+                for (int i = 0; i < record.length; i++) {
+                    record[i] = results.getObject(orderedColumns[i]).toString();
+                }
+                records.add(record);
+            }
 
-        
-        return null;
+            // now copy the records into an array
+            String[][] array = new String[records.size()][orderedColumns.length];
+            for (int i = 0; i < array.length; i++) {
+                String[] record = records.get(i);
+                System.arraycopy(record, 0, array[i], 0, record.length);
+            }
+
+            WsfResult wsfResult = new WsfResult();
+            wsfResult.setResult(array);
+            return wsfResult;
+        } finally {
+            SqlUtils.closeResultSet(results);
+        }
     }
 }
