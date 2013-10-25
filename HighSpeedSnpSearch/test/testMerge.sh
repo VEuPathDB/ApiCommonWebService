8c4d7266753e94c0e2026a77393793649d2bddcb
Steve Fischer <sfischer@upenn.edu>
2013-10-25 18:12:59 +0000

    clean up
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/trunk@58807 735e2a04-f8fc-0310-8a1b-f2942603c481

--- HighSpeedSnpSearch/test/testMerge.sh
+++
@@ -1,7 +0,0 @@
-set -e
-mkfifo fifo1 fifo2
-perl ../strainsTextToBinary 1 < strain1.txt > fifo1 &
-perl ../strainsTextToBinary 2 < strain2.txt > fifo2 &
-echo hello
-../mergeStrains fifo1 1 fifo2 2 | ../dumpStrains 0
-rm fifo1 fifo2
