39fb7a15b768c54ef8a74759a9ecf7e132f62302
Steve Fischer <sfischer@upenn.edu>
2013-12-05 18:40:26 +0000

    debug diploid code; fix up test suite
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/trunk@59325 735e2a04-f8fc-0310-8a1b-f2942603c481

--- HighSpeedSnpSearch/src/c/hsssFindPolymorphic.c
+++ HighSpeedSnpSearch/src/c/hsssFindPolymorphic.c
@@ -3,24 +3,24 @@
 #include <string.h> 
 
 // using global variables to cut down on stack pushing operations.
-FILE *f1;
-FILE *f2;
+FILE *strainFile;
+FILE *refFile;
 int polymorphismThreshold;
 int unknownsThreshold;
 int strainCount;
 char *sourceIdPrefix;
 
 // input strains
-int16_t seq1;
-int16_t *seq1_p = &seq1;
-int32_t loc1;
-int32_t *loc1_p = &loc1;
-char a1;  // allele
-char *a1_p = &a1;
-char p1;  // product
-char *p1_p = &p1;
-int8_t strain1;  
-int8_t *strain1_p = &strain1;
+int16_t seq = -1;
+int16_t *seq_p = &seq;
+int32_t loc = -1;
+int32_t *loc_p = &loc;
+char allele = -1;  
+char *allele_p = &allele;
+char product = -1;  
+char *product_p = &product;
+int8_t strain = -1;  
+int8_t *strain_p = &strain;
 
 // reference genome
 int16_t refSeq = 0;
@@ -37,42 +37,54 @@ int c_count = 0;
 int g_count = 0;
 int t_count = 0;
 int U_count = 0; // unknown
+int diploidCount = 0; // for a given SNP, the number of variants that are diploid.  incremented for each variant beyond the first per strain.
 int sumCount = 0;
-int prevProduct;
+
+int prevProduct = -1;
+int prevStrain = -1;
 int nonSyn = 0;
+int16_t prevSeq;
+int32_t prevLoc;
+
 
 static inline int readStrainRow() {
-	fread(seq1_p, 2, 1, f1);  
-	fread(loc1_p, 4, 1, f1);  
-	fread(a1_p, 1, 1, f1); 
-	fread(p1_p, 1, 1, f1);
-	return fread(strain1_p, 2, 1, f1);
+	prevSeq = seq;
+	prevLoc = loc;
+	if (product > 0) prevProduct = product;  // remember last known product
+	prevStrain = strain;
+
+	fread(seq_p, 2, 1, strainFile);  
+	fread(loc_p, 4, 1, strainFile);  
+	fread(allele_p, 1, 1, strainFile); 
+	fread(product_p, 1, 1, strainFile);
+	return fread(strain_p, 2, 1, strainFile);
 }
 
 static inline getRefGenomeInfo(int16_t seq, int32_t loc) {
 	// refGenome file is a strain file: one row per SNP, showing the ref genomes values
 	// advance through SNPs to our current one
 	while(refSeq != seq && refLoc != loc) {
-		fread(refSeq_p, 2, 1, f2);  
-		fread(refLoc_p, 4, 1, f2);  
-		fread(refAllele_p, 1, 1, f2); 
-		fread(refProduct_p, 1, 1, f2);
+		fread(refSeq_p, 2, 1, refFile);  
+		fread(refLoc_p, 4, 1, refFile);  
+		fread(refAllele_p, 1, 1, refFile); 
+		fread(refProduct_p, 1, 1, refFile);
 	}
 }
 
 static inline updateCounts() {
-	if (a1 == 0 && p1 == -1) {
-		U_count += strain1;
-		sumCount += strain1;
+	if (allele == 0 && product == -1) {
+		U_count += strain;
+		sumCount += strain;
 	} else {
-		if (a1 == 1) a_count++;
-		else if (a1 == 2) c_count++;
-		else if (a1 == 3) g_count++;
-		else if (a1 == 4) t_count++;
+		if (allele == 1) a_count++;
+		else if (allele == 2) c_count++;
+		else if (allele == 3) g_count++;
+		else if (allele == 4) t_count++;
 		else U_count++;
 		sumCount++;
+		if (strain == prevStrain) diploidCount++;  // more than one variant for this strain.  count as a diploid variant
 	}
-	if (p1 != prevProduct && p1 > 0 && prevProduct > 0) nonSyn = 1;
+	if (product != prevProduct && product > 0 && prevProduct > 0) nonSyn = 1;
 }
 
 main(int argc, char *argv[]) {
@@ -85,55 +97,46 @@ main(int argc, char *argv[]) {
 	polymorphismThreshold = atoi(argv[4]);
 	unknownsThreshold = atoi(argv[5]);
 
-	f1 = fopen(argv[1], "rb");
-	if (f1 == 0) {
+	strainFile = fopen(argv[1], "rb");
+	if (strainFile == 0) {
 		fprintf(stderr, "Can't open strainFile '%s' \n", argv[1] );
 		return -1;
 	}
 
-	f2 = fopen(argv[2], "rb");
-	if (f2 == 0) {
+	refFile = fopen(argv[2], "rb");
+	if (refFile == 0) {
 		fprintf(stderr,  "Can't open refGenomeFile '%s' \n", argv[2] );
 		return -1;
 	}
 
-	int f1got;
-	int16_t prevSeq;
-	int32_t prevLoc;
+	int strainFileGot;
 
 	// prime things by reading, but not processing, first SNP in the input
-	f1got = readStrainRow();
-	prevSeq = seq1;
-	prevLoc = loc1;
-	if (p1 > 0) prevProduct = p1;
-
-	while (seq1 == prevSeq && loc1 == prevLoc && f1got != 0) {
+	strainFileGot = readStrainRow();
+	prevSeq = seq;
+	prevLoc = loc;
+	while (seq == prevSeq && loc == prevLoc && strainFileGot != 0) {
 		updateCounts();
-		f1got = readStrainRow();		
+		strainFileGot = readStrainRow();		
 	}
 
-	// read and process all the rest of the SNPs in the input
-	while(f1got != 0) {
+	// read and process the rest of the SNPs in the input
+	while(strainFileGot != 0) {
 
 		// first process prev SNP and clear counts
-		//fprintf(stderr, "%i %i %i %i\n", seq1, prevSeq, loc1, prevLoc);
-		if (seq1 != prevSeq || loc1 != prevLoc) processPreviousSnp(prevSeq, prevLoc);
+		// fprintf(stderr, "%i %i %i %i\n", seq, prevSeq, loc, prevLoc);
+		if (seq != prevSeq || loc != prevLoc) processPreviousSnp(prevSeq, prevLoc);
 
 		// update counts with this variant
 		updateCounts();
 
-		// remember this variant as previous
-		prevSeq = seq1;
-		prevLoc = loc1;
-		if (prevProduct > 0) prevProduct = p1;
-
 		// read next variant
-		f1got = readStrainRow();
+		strainFileGot = readStrainRow();
 	}	
 	processPreviousSnp(prevSeq, prevLoc); // process final snp
 
-	fclose(f1);
-	fclose(f2);
+	fclose(strainFile);
+	fclose(refFile);
 	return 0;
 }
 
@@ -148,9 +151,16 @@ processPreviousSnp(int32_t prevSeq, int32_t prevLoc) {
 
 		// get reference genome allele and product for this SNP
 		getRefGenomeInfo(prevSeq, prevLoc);
-		int ref_count = strainCount - sumCount; // sum_count includes unknowns
+
+		int ref_count = (strainCount + diploidCount) - sumCount; // sumCount includes unknowns
 
 		if (ref_count > 0 && refProduct != prevProduct && prevProduct > 0) nonSyn = 1;  // we saw some ref alleles, might have a second product
+
+		// add in ref allele
+		if (refAllele == 1) a_count += ref_count;
+		else if (refAllele == 2) c_count += ref_count;
+		else if (refAllele == 3) g_count += ref_count;
+		else if (refAllele == 4) t_count += ref_count;
 
 		// find major allele
 		int *majorCount;
@@ -158,15 +168,14 @@ processPreviousSnp(int32_t prevSeq, int32_t prevLoc) {
 		if (c_count > *majorCount) majorCount = &c_count;
 		if (g_count > *majorCount) majorCount = &g_count;
 		if (t_count > *majorCount) majorCount = &t_count;
-		if (ref_count > *majorCount) majorCount = &ref_count;
 
 		// write it out if has enough polymorphisms
-		int polyMorphisms = strainCount - U_count - *majorCount;
-		int polyMorphismsPercent = (polyMorphisms * 100) / (strainCount - U_count);
+		int polymorphisms = strainCount - U_count - *majorCount;
+		int polymorphismsPercent = (polymorphisms * 100) / (strainCount - U_count);
 		int knownPercent = (strainCount - U_count) * 100 / strainCount;
 
-		if (polyMorphisms >= polymorphismThreshold) {
-			printf("%i\t%i\t%i\t%i\t%i\n", prevSeq, prevLoc, knownPercent, polyMorphismsPercent, nonSyn);
+		if (polymorphisms >= polymorphismThreshold) {
+			printf("%i\t%i\t%i\t%i\t%i\n", prevSeq, prevLoc, knownPercent, polymorphismsPercent, nonSyn);
 		}
 	}
 
@@ -178,6 +187,11 @@ processPreviousSnp(int32_t prevSeq, int32_t prevLoc) {
 	U_count = 0;
 	sumCount = 0;
 	nonSyn = 0;
+	diploidCount = 0;
+
+	// reset prev for new snp
+	prevProduct = -1;
+	prevStrain = -1;
 }
 
 
