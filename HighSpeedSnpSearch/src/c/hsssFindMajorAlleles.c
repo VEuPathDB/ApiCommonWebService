2a2c2506f19b1a22137b0b3f951ef7aa7b40acb6
Steve Fischer <sfischer@upenn.edu>
2014-01-26 02:56:01 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/trunk@60160 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ HighSpeedSnpSearch/src/c/hsssFindMajorAlleles.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	int16_t seq;
+	int32_t loc;
+	char majorAllele;  
+	char majorProduct;  
+	char majorProductIsVariable;  
+	char minorAllele;  
+	char minorProduct;  
+	char minorProductIsVariable;  
+	int16_t majorAllelePerTenThou;
+	int16_t minorAllelePerTenThou;
+	char isTriallelic;
+} snp;
+
+static char *stdoutStr = "STDOUT";
+
+static inline int freadCheck (char *filename, void *ptr, size_t size, size_t count, FILE *stream) {
+	int bytes = fread(ptr, size, count,stream);
+	if (ferror(stream)) {
+		fprintf(stderr, "Failed reading file '%s' \n", filename );
+	 	exit(-1);
+	}
+	return bytes;
+}
+
+static inline int readSnp(FILE *stream, snp *snp, char *filename) {
+	int16_t tmp;
+
+	freadCheck(filename, &(*snp).seq, 2, 1, stream);
+	freadCheck(filename, &(*snp).loc, 4, 1, stream);
+	freadCheck(filename, &(*snp).majorAllele, 1, 1, stream);
+	freadCheck(filename, &(*snp).majorProduct, 1, 1, stream);
+	freadCheck(filename, &(*snp).majorProductIsVariable, 1, 1, stream);
+	freadCheck(filename, &(*snp).minorAllele, 1, 1, stream);
+	freadCheck(filename, &(*snp).minorProduct, 1, 1, stream);
+	freadCheck(filename, &(*snp).minorProductIsVariable, 1, 1, stream);
+	freadCheck(filename, &(*snp).majorAllelePerTenThou, 2, 1, stream);
+	freadCheck(filename, &(*snp).minorAllelePerTenThou, 2, 1, stream);
+	return freadCheck(filename, &(*snp).isTriallelic, 1, 1, stream);
+}
+
+static inline void reportSnp(snp snp1, snp snp2) {
+	printf("%i\t%i\t%i\t%i\t%i\t%i\t%i\t%i\t%i\t%i\t%i\t%i\n", snp1.seq, snp1.loc, snp1.majorAllele, snp1.majorProduct, snp1.majorProductIsVariable, snp1.majorAllelePerTenThou, snp1.isTriallelic, snp2.majorAllele, snp2.majorProduct, snp2.majorProductIsVariable, snp2.majorAllelePerTenThou, snp2.isTriallelic);
+
+}
+
+main(int argc, char *argv[]) {
+	FILE *f1;
+	FILE *f2;
+	snp snp1;
+	snp snp2;
+
+	if ( argc != 3 ) {
+		fprintf(stderr, "usage: %s consensus_file1 consensus_file2\n\n", argv[0] );
+		return -1;
+	}
+
+	f1 = fopen(argv[1], "rb");
+	if (f1 == 0) {
+		fprintf(stderr, "Can't open file1 '%s' \n", argv[1] );
+		return -1;
+	}
+
+	f2 = fopen(argv[2], "rb");
+	if (f2 == 0) {
+		fprintf(stderr,"Can't open file2 '%s' \n", argv[2] );
+		return -1;
+	}
+
+	int f1got;
+	int f2got;
+	f1got = readSnp(f1, &snp1, argv[1]);
+	f2got = readSnp(f2, &snp2, argv[2]);
+
+	while(f1got != 0 && f2got != 0) {
+		// skip snps in file 1 absent from file 2
+		while ((snp1.seq < snp2.seq || (snp1.seq == snp2.seq && snp1.loc < snp2.loc)) && f1got != 0) { 
+			f1got = readSnp(f1, &snp1, argv[1]);
+		}
+		// skip snps in file 2 absent from file 1
+		while ((snp2.seq < snp1.seq || (snp2.seq == snp1.seq && snp2.loc < snp1.loc)) && f2got != 0) {
+			f2got = readSnp(f2, &snp2, argv[2]);
+		}
+		if (snp1.seq == snp2.seq && snp1.loc == snp2.loc) {
+			if (snp1.majorAllele != snp2.majorAllele) reportSnp(snp1, snp2);
+			f1got = readSnp(f1, &snp1, argv[1]);
+			f2got = readSnp(f2, &snp2, argv[2]);
+		}
+	}
+	fclose(f1);
+	fclose(f2);
+	return 0;
+}
+
+
