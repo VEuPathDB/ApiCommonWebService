f5283d559966cf9eb50ec4145bb7dbca2c269b61
Steve Fischer <sfischer@upenn.edu>
2014-02-26 15:16:35 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/trunk@60682 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/highspeedsnpsearch/FindSnpsByGenomicLocationPlugin.java
@@ -0,0 +1,62 @@
+package org.apidb.apicomplexa.wsfplugin.highspeedsnpsearch;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.gusdb.fgputil.runtime.GusHome;
+import org.gusdb.wsf.plugin.PluginRequest;
+import org.gusdb.wsf.plugin.WsfPluginException;
+
+/**
+ * @author steve
+ */
+public class FindSnpsByGenomicLocationPlugin extends FindPolymorphismsPlugin {
+
+  // required parameter definition
+  public static final String PARAM_GENES_DATASET = "ds_gene_ids";
+
+  public static final String genomicLocationsFileName = "genomicLocations.txt";
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.gusdb.wsf.plugin.WsfPlugin#getRequiredParameterNames()
+   */
+  @Override
+  public String[] geExtraParamNames() {
+    return new String[] {PARAM_GENE_IDS};
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.gusdb.wsf.plugin.WsfPlugin#validateParameters(java.util.Map)
+   */
+  @Override
+  public void validateParameters(PluginRequest request)
+    throws WsfPluginException {
+  }
+
+  @Override
+  protected void initForBashScript(File jobDir, Map<String, String> params, File organismDir) throws WsfPluginException {
+    String gene_list_dataset = params.get(PARAM_GENES_DATASET);
+  }
+
+  @Override
+  protected List<String> makeCommandToCreateBashScript(File jobDir, Map<String, String> params, File organismDir) throws WsfPluginException {
+    String chromosome = params.get(PARAM_CHROMOSOME);
+    String seq = params.get(PARAM_SEQUENCE);
+    if (seq.equals("")) seq = chromosome;
+    String start = params.get(PARAM_START_POINT);
+    int end = Integer.parseInt(params.get(PARAM_END_POINT));
+    if (end == 0) end = 1000000000;
+    List<String> command = super.makeCommandToCreateBashScript(jobDir, params, organismDir);
+    command.add(seq);
+    command.add(start);
+    command.add("" + end);
+    return command;
+  }
+ 
+}
