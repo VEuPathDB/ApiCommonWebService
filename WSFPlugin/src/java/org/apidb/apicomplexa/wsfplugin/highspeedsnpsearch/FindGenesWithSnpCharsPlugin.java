bc34ba92882bd070cbf7578174ec0296ede76621
Steve Fischer <sfischer@upenn.edu>
2014-03-14 14:45:05 +0000

    add init method
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/trunk@60884 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/highspeedsnpsearch/FindGenesWithSnpCharsPlugin.java
+++ WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/highspeedsnpsearch/FindGenesWithSnpCharsPlugin.java
@@ -42,6 +42,67 @@ public class FindGenesWithSnpCharsPlugin extends FindPolymorphismsPlugin {
   }
 
   @Override
+  protected void initForBashScript(File jobDir, Map<String, String> params, File organismDir) throws WsfPluginException {
+    String gene_list_dataset = params.get(PARAM_GENES_DATASET);
+    File filtersFile = new File(jobDir, genomicLocationsFileName);
+    BufferedWriter bw = null;
+    try {
+      if (!filtersFile.exists()) filtersFile.createNewFile();
+      FileWriter w = new FileWriter(filtersFile);
+      bw = new BufferedWriter(w);
+      if (gene_list_dataset.equals("unit test")) {
+	String[] testFilters = new String[] {"e99\t1000\t3000\tg1", "f100\t500\t700\tg2", "h103\t30021\t40000\tg3", "j201\t20\t50\tg4"};
+	for (String filter : testFilters ) {
+	  bw.write(filter);
+	  bw.newLine();
+	}
+      } else {
+	WdkModelBean wdkModelBean = (WdkModelBean)context.get(CTX_CONTAINER_APP);
+	WdkModel wdkModel = wdkModelBean.getModel();
+	DataSource dataSource = wdkModel.getAppDb().getDataSource();
+	String newline = System.lineSeparator();
+
+	String organism = removeSingleQuotes(params.get(PARAM_ORGANISM));
+
+	// can interpolate organism into sql w/o fear of injection because it came from a vocabulary param
+	String sql = "select g.sequence_id, g.start_min, g.end_max, g.source_id" + newline +
+	  "from apidbtuning.geneattributes g, " + newline +
+	  "where g.source_id is not null" + newline +
+          " and g.organism = '" + organism + "'" + newline +
+	  "order by g.sequence_id, g.start_min, g.end_max";
+   
+	ResultSet rs = null;
+
+	try {
+	  rs = SqlUtils.executeQuery(dataSource, sql, "FindGenesWithSnpCharsPlugin");
+
+	  while (rs.next()) {
+	    String sourceId = rs.getString(1);
+	    String start = rs.getString(2);
+	    String end = rs.getString(3);
+	    bw.write(sourceId + "\t" + start + "\t" + end);
+	    bw.newLine();
+	  }
+	  logger.info("finished fetching rows");
+
+	} catch (Exception ex) {
+	  throw new WsfPluginException(ex);
+	} finally {
+	  SqlUtils.closeResultSetAndStatement(rs);
+	}
+      }
+    } catch (IOException e) {
+      throw new WsfPluginException("Failed writing to file" + filtersFile, e);
+    } finally {
+      try {
+        if (bw != null) bw.close();
+      } catch (IOException e) {
+        throw new WsfPluginException("Failed closing file" + filtersFile,  e);
+      }
+    }
+  }
+
+  @Override
   protected List<String> makeCommandToCreateBashScript(File jobDir, Map<String, String> params, File organismDir) throws WsfPluginException {
     String snpClass = params.get(PARAM_SNP_CLASS);
     String min  = params.get(PARAM_OCCURENCES_LOWER);
