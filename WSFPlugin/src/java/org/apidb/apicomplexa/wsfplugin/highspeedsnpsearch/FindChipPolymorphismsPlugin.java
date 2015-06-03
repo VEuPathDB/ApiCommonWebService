59aa5b65a54746775602c02a33005f12ece89f9a
Ja’Shon Cade <jcade@upenn.edu>
2015-06-03 22:38:49 +0000

    added abstract superclass to split snp and snp chip queries
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/trunk@68210 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/highspeedsnpsearch/FindChipPolymorphismsPlugin.java
@@ -0,0 +1,88 @@
+package org.apidb.apicomplexa.wsfplugin.highspeedsnpsearch;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.gusdb.fgputil.ArrayUtil;
+import org.gusdb.fgputil.runtime.GusHome;
+import org.gusdb.wsf.plugin.PluginModelException;
+import org.gusdb.wsf.plugin.PluginRequest;
+import org.gusdb.wsf.plugin.PluginResponse;
+import org.gusdb.wsf.plugin.PluginUserException;
+import org.apache.log4j.Logger;
+
+/**
+ * @author steve
+ */
+public class FindChipPolymorphismsPlugin extends FindPolymorphismsAbstractPlugin {
+
+    @SuppressWarnings("unused")
+     private static final Logger logger = Logger.getLogger(FindChipPolymorphismsPlugin.class);
+    
+    // required parameter definition
+    public static final String PARAM_ASSAY_TYPE = "snp_assay_type";
+
+    // required result column definition
+    public static final String COLUMN_PERCENT_OF_POLYMORPHISMS = "PercentMinorAlleles";
+    public static final String COLUMN_PERCENT_OF_KNOWNS = "PercentIsolateCalls";
+    public static final String COLUMN_PHENOTYPE = "Phenotype";
+
+
+
+    @SuppressWarnings("unused")
+        private static final String JOBS_DIR_PREFIX = "hsssFindChipPolymorphisms.";
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.gusdb.wsf.plugin.WsfPlugin#getColumns()
+     */
+
+    private static final String propertyFile = "highSpeedChipSnpSearch-config.xml";
+    
+    public FindChipPolymorphismsPlugin() {
+    super(propertyFile);
+  }
+
+  @Override
+        protected String getSearchDir() {
+        return  "/highSpeedChipSnpSearch";
+  }
+
+  @Override
+      protected String getPARAM_STRAIN_LIST() {
+      return "snpchip_strain_meta";
+  }
+
+    @Override
+        protected String getJobsDirPrefix() {
+        return "hsssChipFindPolymorphisms.";
+    }
+
+    @Override
+        protected String getResultsFileBaseName() {
+        return "results";
+    }
+
+    @Override
+     protected List<String> makeCommandToCreateBashScript(File jobDir, Map<String, String> params,
+                                                          File organismDir) throws PluginUserException, PluginModelException {
+
+        List<String> command = super.makeCommandToCreateBashScript(jobDir, params,organismDir);
+        String type = params.get(PARAM_ASSAY_TYPE);
+        String suffix = type.replace("Broad_",".");
+        command.add(suffix);       
+        logger.info("running command " + command.toString() + " from CHip Polymorph"); 
+        return command;
+    }
+    
+
+    /**
+     * @throws WsfPluginException
+     * @throws PluginModelException
+     */
+
+ 
+}
