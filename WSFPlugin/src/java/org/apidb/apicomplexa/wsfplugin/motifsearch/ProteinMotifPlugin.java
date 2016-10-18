ffe46a66f8d097572b88f6be577dd81c1014b3fe
Steve Fischer <sfischer@upenn.edu>
2016-10-18 16:36:26 +0000

    fix orf motif search gus4 bug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/branches/api-build-29@77757 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/motifsearch/ProteinMotifPlugin.java
+++ WSFPlugin/src/java/org/apidb/apicomplexa/wsfplugin/motifsearch/ProteinMotifPlugin.java
@@ -3,13 +3,7 @@
  */
 package org.apidb.apicomplexa.wsfplugin.motifsearch;
 
-import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.log4j.Logger;
 import org.gusdb.wsf.plugin.PluginModelException;
 import org.gusdb.wsf.plugin.PluginResponse;
 import org.gusdb.wsf.plugin.PluginUserException;
@@ -20,16 +14,10 @@ import org.gusdb.wsf.plugin.PluginUserException;
  */
 
 // geneID could be an ORF or a genomic sequence deending on who uses the plugin
-public class ProteinMotifPlugin extends AbstractMotifPlugin {
+public class ProteinMotifPlugin extends AAMotifPlugin {
 
-  // let's store files in same directory
-  private static final String FIELD_REGEX = "ProteinDeflineRegex";
-  private static final String DEFAULT_REGEX = ">(?:\\w*\\|)*([^|\\s]+)\\s*\\|.*?\\s*organism=([^|\\s]+)";
-
-  private static final Logger logger = Logger.getLogger(ProteinMotifPlugin.class);
-
-	public static final String COLUMN_GENE_SOURCE_ID = "gene_source_id";
-	public static final String COLUMN_MATCHED_RESULT = "matched_result";
+  public static final String COLUMN_GENE_SOURCE_ID = "gene_source_id";
+  public static final String COLUMN_MATCHED_RESULT = "matched_result";
 
   public ProteinMotifPlugin() {
     super(FIELD_REGEX, DEFAULT_REGEX);
@@ -58,25 +46,6 @@ public class ProteinMotifPlugin extends AbstractMotifPlugin {
   }
 
   @Override
-  protected Map<Character, String> getSymbols() {
-    Map<Character, String> symbols = new HashMap<Character, String>();
-    symbols.put('0', "DE");
-    symbols.put('1', "ST");
-    symbols.put('2', "ILV");
-    symbols.put('3', "FHWY");
-    symbols.put('4', "KRH");
-    symbols.put('5', "DEHKR");
-    symbols.put('6', "AVILMFYW");
-    symbols.put('7', "KRHDENQ");
-    symbols.put('8', "CDEHKNQRST");
-    symbols.put('9', "ACDGNPSTV");
-    symbols.put('B', "AGS");
-    symbols.put('Z', "ACDEGHKNQRST");
-
-    return symbols;
-  }
-
-  @Override
   protected void addMatch(PluginResponse response, Match match,
       Map<String, Integer> orders) throws PluginModelException, PluginUserException  {
     String[] result = new String[orders.size()];
@@ -90,99 +59,4 @@ public class ProteinMotifPlugin extends AbstractMotifPlugin {
     response.addRow(result);
   }
 
-  @Override
-  protected void findMatches(PluginResponse response,
-      Map<String, Integer> orders, String headline, Pattern searchPattern,
-      String sequence) throws PluginModelException, PluginUserException {
-    MotifConfig config = getConfig();
-
-    // parse the headline
-    Matcher deflineMatcher = config.getDeflinePattern().matcher(headline);
-    if (!deflineMatcher.find()) {
-      logger.warn("Invalid defline: " + headline + " Against Pattern "
-          + config.getDeflinePattern().pattern());
-      return;
-    }
-    // the gene source id has to be in group(1),
-    // organism has to be in group(2),
-    String sourceId = deflineMatcher.group(1);
-    String organism = deflineMatcher.group(2).replace('_', ' ');
-
-    // workaround: trim "-p1" suffix to turn protein ID into transcript ID
-    sourceId = sourceId.replace("-p1", "");
-
-    Match match = new Match();
-    match.sourceId = sourceId;
-    try {
-      match.projectId = getProjectId(organism);
-    } catch (SQLException ex) {
-      throw new PluginModelException(ex);
-    }
-
-    StringBuffer sbLoc = new StringBuffer();
-    StringBuffer sbSeq = new StringBuffer();
-    int prev = 0;
-    int contextLength = config.getContextLength();
-
-    Matcher matcher = searchPattern.matcher(sequence);
-    boolean longLoc = false, longSeq = false;
-    while (matcher.find()) {
-      // add locations only while we have room.
-      if (!longLoc) {
-        String location = getLocation(0, matcher.start(), matcher.end() - 1,
-            false);
-        location = "(" + location + ")";
-        if (sbLoc.length() + location.length() >= 3997) {
-          sbLoc.append("...");
-          longLoc = true;
-        } else {
-          if (sbLoc.length() != 0) sbLoc.append(", ");
-          sbLoc.append(location);
-        }
-      }
-
-      // add sequences only while we have room.
-      if (!longSeq) {
-        StringBuilder seq = new StringBuilder();
-        // obtain the context sequence
-        if ((matcher.start() - prev) <= (contextLength * 2)) {
-          // no need to trim
-          seq.append(sequence.substring(prev, matcher.start()));
-        } else { // need to trim some
-          if (prev != 0)
-            seq.append(sequence.substring(prev, prev + contextLength));
-          seq.append("... ");
-          seq.append(sequence.substring(matcher.start() - contextLength,
-              matcher.start()));
-        }
-        seq.append("<span class=\"" + MOTIF_STYLE_CLASS + "\">");
-        seq.append(sequence.substring(matcher.start(), matcher.end()));
-        seq.append("</span>");
-
-        // determine if we have enough space for the new sequence
-        if (sbSeq.length() + seq.length() >= 3997) {
-          sbSeq.append("...");
-          longSeq = true;
-	} else {
-          sbSeq.append(seq);
-        }
-      }
-
-      prev = matcher.end();
-      match.matchCount++;
-    }
-    if (match.matchCount == 0) return;
-
-    // grab the last context
-    if (!longSeq) {
-      String remain = ((prev + contextLength) < sequence.length()) 
-          ? sequence.substring(prev, prev + contextLength) + "..."
-          : sequence.substring(prev);
-      if (remain.length() + sbSeq.length() < 4000)
-        sbSeq.append(remain);
-    }
-    match.locations = sbLoc.toString();
-    match.sequence = sbSeq.toString();
-    addMatch(response, match, orders);
-  }
 }
