bc8320b11ce4ae11963ebc11f73f2c4bdead55c8
Lin Xu <linxu123@upenn.edu>
2019-01-30 20:25:46 +0000

    change genomic background and 2x2 contingency table cell counts calculation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonWebService/trunk@94907 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/lib/perl/fisherExactTest.pl
+++ WSFPlugin/lib/perl/fisherExactTest.pl
@@ -20,14 +20,11 @@ my $dbPassword = $ARGV[5];
 # setup DBI connections
 my $dbh = DBI->connect($dbConnection, $dbLogin, $dbPassword) or die "Unable to connect: DBI->errstr\n";
 
-
-
 ########################################################## userlist hash table ##################################################
 
 my $idT = (lc($useOrthology) eq "yes") ? "orthomcl_name" : "source_id";
 
-
-my $userGeneListQuery = "select distinct ga." . $idT . ", ga.organism from apidbtuning.geneattributes ga, (" . $idSql . ") id where id.gene_source_id = ga.source_id";
+my $userGeneListQuery = "select distinct ga." . $idT . ", ga.organism from apidbtuning.geneattributes  ga, (" . $idSql . ") id where id.gene_source_id = ga.source_id";
 
 #print STDERR $userGeneListQuery . "\n";
 
@@ -45,48 +42,64 @@ while(my ($id,$org) = $userStatemnetHandle->fetchrow_array() ) {
 
 $userStatemnetHandle->finish();
 
-
-########################################################## dataset list hash table ###############################################
+########################################################## user dataset list hash table ###########################################
 
 my $datasetGeneListQuery = "select distinct ga." . $idT . ", ga.organism, ga.dataset_presenter_id from apidbtuning.datasetgenelist ga where ga.fdiff_abs > $FC";
 
 my $datasetStatmentHandle = $dbh->prepare($datasetGeneListQuery);                                                                   
 $datasetStatmentHandle->execute();
+
 my %datasetLists;                                                                                                                  
 
 while(my($id, $org, $dataset) = $datasetStatmentHandle->fetchrow_array() ) {                                             
 
-    $datasetLists{$org}->{$dataset}->{$id}++;                                                                                       
+    $datasetLists{$org}->{$dataset}->{$id}++;                                                                                      
+
 }                                                                                                                                  $datasetStatmentHandle->finish();  
 
-#print STDERR Dumper \%datasetLists;
+########################################################## Background Dataset list hash table #####################################
 
+my $backgroundDatasetQuery = "select distinct ga." . $idT . ", ga.dataset_presenter_id from apidbtuning.datasetgenelist ga";
+
+my $backgroundDatasetStatmentHandle = $dbh->prepare($backgroundDatasetQuery);                                                                   
+$backgroundDatasetStatmentHandle->execute();
+
+my %backgroundDSLists;                                                                                                                  
+
+while(my($id,$dataset) = $backgroundDatasetStatmentHandle->fetchrow_array() ) {                                             
+
+    $backgroundDSLists{$dataset}->{$id}++;                                                                                      
+
+}                                                                                                                                  $backgroundDatasetStatmentHandle->finish();  
 
 ######################################################### combined calculation ### ###############################################
 
-
-###################################### give each of those columns a colname
-
-print "dataset_id", "\t", "overlap","\t", "ul_nonDS","\t", "ds_nonUL". "\t", "nonUL_nonDS", "\t", "p_value", "\n";
+#print "dataset_id", "\t", "overlap","\t", "ul_nonDS","\t", "ds_nonUL". "\t", "nonUL_nonDS", "\t", "p_value", "\n";
 
 foreach my $org (keys %userLists){
-    my $backgroundSize = &getBackgroundForOrganism($dbh, $org, $idT); ########## background SIZE                       
+
     my @userIdList = keys %{$userLists{$org}}; 
     my $userListSize = scalar @userIdList;   ############################### user_list SIZE
 
-    # make a hash from the userIdList Array
+    #make a hash from the userIdList Array, keys are the elements from @userIdList
     my %userIdListHash = map { $_ => 1 } @userIdList;
 
     foreach my $dataset (keys %{$datasetLists{$org}}){
+
+	my $backgroundSize = &getBackgroundForOrganism($dbh, $dataset, $idT); ########## background dataset SIZE 
+
 	my  @datasetIdList = keys %{$datasetLists{$org}->{$dataset}};
 
-	my $datasetListSize  =  scalar @datasetIdList; ####################### dataset_list SIZE
+	my $datasetListSize  =  scalar @datasetIdList; ####################### User_dataset_list SIZE
+	
+	##### background dataset geneIDs array (this is used for the calculation of $t21)
+	my  @backgroundDatasetIdList = keys %{$backgroundDSLists{$dataset}};
 	
 	my $t11 = 0; 
 	my $t21 = 0; 
 	my $t12 = 0; 
 	my $t22 = 0;
-       
+	my $tt = 0;
 
 	# Find the number of overlap between two lists
 	foreach my $value1 (@datasetIdList){
@@ -96,21 +109,23 @@ foreach my $org (keys %userLists){
 	}
 
 	$t12 = $datasetListSize - $t11;
-	$t21 = $userListSize - $t11;
+
+	foreach my $value2 (@backgroundDatasetIdList){
+	    if($userIdListHash{$value2}){
+		$tt++;
+	    }
+	}
+
+	$t21 = $tt - $t11;
 	$t22 = $backgroundSize - $t11 - $t12 -$t21;
-
-	#print STDERR $t22 . "\n";
-	
-
 
 	my $pValue = &runRscript($t11, $t21, $t12, $t22);
 
 
-# NOTE: if we need to restrict by pvalue, we should read this as a param, not use 0.1 hard coded	
-#	if ($pValue < 0.1){
-            print  $dataset,"\t",$t11,"\t",$t21, "\t", $t12, "\t", $t22, "\t", $pValue, "\n";
-#	}
-
+###############################################################################################################
+##### the 'print' values order should match up the headers order in ListComparionPlugin.java
+###############################################################################################################
+ print  $dataset,"\t",$t11,"\t",$t21, "\t", $t12, "\t", $t22, "\t", $pValue, "\n";
 
     }
 }
@@ -119,23 +134,19 @@ sub runRscript{
     my ($t11,$t21,$t12,$t22) = @_;
     
     my $rCode = <<"RCODE";
-
     Test <-matrix(c($t11,$t21,$t12,$t22), nrow = 2, dimnames = list(DS = c("DS", "Non-DS"),UL = c("UL", "Non-UL")))
     fisher.test(Test, alternative = "greater")\$p
-
 RCODE
 
     my ($FH, $file) = tempfile(SUFFIX => '.R');     
-    
     print $FH  $rCode;
-
     my $command = "Rscript " .  $file;
-      
     my $p_value  =  `$command`;
     
+    #print STDERR $p_value;
     close (FH);
     
-    if ($p_value =~ m/^\[1\]\s*([\d]+.+)/) {
+    if ($p_value =~ m/^\[1\]\s*([\d]+.+)/ || $p_value =~ m/^\[1\]\s*([\d]+)/) {
         #print "$1\n";
 	return  $1;
     }
@@ -143,12 +154,12 @@ RCODE
 
 
 sub getBackgroundForOrganism{
-    my ($dbh, $org, $idType) = @_;
+    my ($dbh, $dataset, $idType) = @_;
 
     my $sql = <<EOSQL;
      select count(distinct $idType) 
-     from  APIDBTUNING.GENEATTRIBUTES  
-     where organism = '$org'
+     from  apidbtuning.datasetgenelist  
+     where dataset_presenter_id = '$dataset'
 EOSQL
     
     my $sth = $dbh->prepare($sql);
