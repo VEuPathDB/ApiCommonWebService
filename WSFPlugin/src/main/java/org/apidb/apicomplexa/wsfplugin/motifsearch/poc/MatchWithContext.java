52d0621a985203295c342efbe19a0bba1bc528ee
Dan Galdi <dmgaldi@upenn.edu>
2022-03-17 09:20:47 -0400

    Added de-duplication of matches

--- WSFPlugin/src/main/java/org/apidb/apicomplexa/wsfplugin/motifsearch/poc/MatchWithContext.java
+++ WSFPlugin/src/main/java/org/apidb/apicomplexa/wsfplugin/motifsearch/poc/MatchWithContext.java
@@ -5,12 +5,21 @@ public class MatchWithContext {
     private String match;
     private String trailingContext;
     private String sequenceId;
+    private int startPos;
+    private int endPos;
 
-    public MatchWithContext(String leadingContext, String match, String trailingContext, String sequenceId) {
+    public MatchWithContext(String leadingContext,
+                            String match,
+                            String trailingContext,
+                            String sequenceId,
+                            int startPos,
+                            int endPos) {
         this.leadingContext = leadingContext;
         this.match = match;
         this.trailingContext = trailingContext;
         this.sequenceId = sequenceId;
+        this.startPos = startPos;
+        this.endPos = endPos;
     }
 
     public String getLeadingContext() {
--- WSFPlugin/src/main/java/org/apidb/apicomplexa/wsfplugin/motifsearch/poc/TileMatcher.java
+++ WSFPlugin/src/main/java/org/apidb/apicomplexa/wsfplugin/motifsearch/poc/TileMatcher.java
@@ -3,16 +3,9 @@ package org.apidb.apicomplexa.wsfplugin.motifsearch.poc;
 import java.io.*;
 import java.nio.CharBuffer;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-
-import java.io.*;
-import java.nio.CharBuffer;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,6 +21,7 @@ public class TileMatcher {
         boolean first = true;
         boolean reachedNewline = false;
         final List<MatchWithContext> matches = new ArrayList<>();
+        final Set<Integer> startPositions = new HashSet<>();
         final SequenceBuffer sequenceBuffer = new SequenceBuffer(MAX_MATCH_LENGTH, contextLength);
         int bytesRead;
         try (final Reader streamReader = new InputStreamReader(sequenceInput);
@@ -39,23 +33,20 @@ public class TileMatcher {
                     // Shift the buffer backward to make space for a BUFFER_SIZE and an overlap window worth of new data
                     sequenceBuffer.shiftBuffers();
                 }
-
-                // Read data into shifted buffer until buffer is full.
-                do {
-                    bytesRead = bufferedReader.read(sequenceBuffer.getCurrentBuffer());
-                } while (sequenceBuffer.getCurrentBuffer().remaining() != 0 && bytesRead != -1);
-
+                bytesRead = sequenceBuffer.read(bufferedReader);
                 String subsequence = sequenceBuffer.readCurrentSubsequence();
 //                if (subsequence.contains(System.lineSeparator())) {
 //                    subsequence = subsequence.split(System.lineSeparator())[0];
 //                    reachedNewline = true;
 //                }
-
                 final Matcher matcher = pattern.matcher(subsequence);
                 while (matcher.find()) {
                     boolean atEnd = bytesRead == -1 || reachedNewline;
                     if (matcher.start() > BUFFER_SIZE + sequenceBuffer.getOverlapWindow() && !atEnd) {
                         break;
+                    }
+                    if (startPositions.contains(matcher.start() + sequenceBuffer.getSequencePosition())) {
+                        continue;
                     }
                     if (matcher.group().length() > MAX_MATCH_LENGTH) {
                         throw new MotifTooLongException("Motif match cannot exceed " + MAX_MATCH_LENGTH + " chars.");
@@ -64,8 +55,11 @@ public class TileMatcher {
                     final String leadingContext = matcher.start() > contextLength
                             ? subsequence.substring(matcher.start() - contextLength, matcher.start())
                             : sequenceBuffer.getLeadingContext(matcher, contextLength);
+                    startPositions.add(matcher.start() + sequenceBuffer.getSequencePosition());
                     matchConsumer.accept(new MatchWithContext(
-                            leadingContext, matcher.group(), trailingContext, sequenceInput.getCurrentSequenceId()));
+                            leadingContext, matcher.group(), trailingContext, sequenceInput.getCurrentSequenceId(),
+                            matcher.start() + sequenceBuffer.getSequencePosition(),
+                            matcher.end() + sequenceBuffer.getSequencePosition()));
                 }
             } while (bytesRead != -1 && !reachedNewline);
         }
@@ -82,10 +76,12 @@ public class TileMatcher {
         private CharBuffer currentBuffer;
         private CharBuffer contextBuffer;
 
-        private int overlapWindow;
-        private int totalBufferSize;
-        private int contextLength;
+        private final int overlapWindow;
+        private final int totalBufferSize;
+        private final int contextLength;
+
         private boolean hasLeadingContext;
+        private int sequencePosition = 0;
 
         public SequenceBuffer(int maxLength, int contextLength) {
             this.overlapWindow =  2 * maxLength; // The leading overlap window only may only need to be equal to contextLength.
@@ -97,20 +93,20 @@ public class TileMatcher {
             currentBuffer = buffer1;
         }
 
+        public int read(BufferedReader reader) throws IOException {
+            int bytesRead;
+            do {
+                bytesRead = reader.read(currentBuffer);
+            } while (currentBuffer.remaining() != 0 && bytesRead != -1);
+            return  bytesRead;
+        }
+
         public String getLeadingContext(Matcher matcher, int contextLength) {
             if (!hasLeadingContext) {
                 return "";
             }
             contextBuffer.position(contextLength - (contextLength - matcher.start()));
             return contextBuffer.toString();
-//            final int position = currentBuffer.position();
-//            final int limit = currentBuffer.limit();
-//            currentBuffer.position((overlapWindow - contextLength + matcher.start()));
-//            currentBuffer.limit((overlapWindow - contextLength + matcher.start()) + contextLength);
-//            String leadingContext = currentBuffer.toString();
-//            currentBuffer.limit(limit);
-//            currentBuffer.position(position);
-//            return leadingContext;
         }
 
         public String readCurrentSubsequence() {
@@ -121,8 +117,8 @@ public class TileMatcher {
             return currentBuffer.toString();
         }
 
-        public CharBuffer getCurrentBuffer() {
-            return currentBuffer;
+        public int getSequencePosition() {
+            return sequencePosition;
         }
 
         public int getOverlapWindow() {
@@ -147,6 +143,7 @@ public class TileMatcher {
                 contextBuffer.put(buffer2.array(), totalBufferSize - contextLength, contextLength);
                 currentBuffer = buffer1;
             }
+            sequencePosition += BUFFER_SIZE + overlapWindow;
             hasLeadingContext = true;
             currentBuffer.position(overlapWindow);
             return currentBuffer;
