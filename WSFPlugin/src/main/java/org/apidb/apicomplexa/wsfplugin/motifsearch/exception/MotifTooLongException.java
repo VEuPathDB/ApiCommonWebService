e1108df6c4f24a01d1b7615be0aed6adbf7f24e6
Dan Galdi <dmgaldi@upenn.edu>
2022-03-21 14:12:28 -0400

    Model motif too long exception

--- WSFPlugin/src/main/java/org/apidb/apicomplexa/wsfplugin/motifsearch/exception/MotifTooLongException.java
+++ WSFPlugin/src/main/java/org/apidb/apicomplexa/wsfplugin/motifsearch/exception/MotifTooLongException.java
@@ -1,9 +1,11 @@
 package org.apidb.apicomplexa.wsfplugin.motifsearch.exception;
 
+import org.gusdb.wsf.plugin.PluginModelException;
+
 /**
  * Exception indicating that a motif match exceeds the maximum length.
  */
-public class MotifTooLongException extends Exception {
+public class MotifTooLongException extends PluginModelException {
     public MotifTooLongException(String message) {
         super(message);
     }
