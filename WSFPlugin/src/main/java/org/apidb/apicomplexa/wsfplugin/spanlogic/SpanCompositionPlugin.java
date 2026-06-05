4f88c69104e92c9d76c18f46ca7b4a6ae9740f0d
steve-fischer-200 <sfischer@upenn.edu>
2026-06-05 17:53:46 -0400

    fix spanlogic bug

--- WSFPlugin/src/main/java/org/apidb/apicomplexa/wsfplugin/spanlogic/SpanCompositionPlugin.java
+++ WSFPlugin/src/main/java/org/apidb/apicomplexa/wsfplugin/spanlogic/SpanCompositionPlugin.java
@@ -332,10 +332,10 @@ public class SpanCompositionPlugin extends AbstractPlugin {
     // and user's choice of begin and end, we get the proper begin of the
     // region.
     StringBuilder sql = new StringBuilder("(CASE ");
-    sql.append(" WHEN NVL(" + table + "is_reversed, 0) = 0 THEN (");
+    sql.append(" WHEN COALESCE(" + table + "is_reversed, 0) = 0 THEN (");
     sql.append(begin.equals(PARAM_VALUE_START) ? "start_min" : "end_max");
     sql.append(" + 1*(" + beginOff + ")) ");
-    sql.append(" WHEN NVL(" + table + "is_reversed, 0) = 1 THEN (");
+    sql.append(" WHEN COALESCE(" + table + "is_reversed, 0) = 1 THEN (");
     sql.append(end.equals(PARAM_VALUE_START) ? "end_max" : "start_min");
     sql.append(" - 1*(" + endOff + ")) ");
     sql.append("END)");
@@ -343,10 +343,10 @@ public class SpanCompositionPlugin extends AbstractPlugin {
 
     // we get the proper end of the region.
     sql = new StringBuilder("(CASE ");
-    sql.append(" WHEN NVL(" + table + "is_reversed, 0) = 0 THEN (");
+    sql.append(" WHEN COALESCE(" + table + "is_reversed, 0) = 0 THEN (");
     sql.append(end.equals(PARAM_VALUE_START) ? "start_min" : "end_max");
     sql.append(" + 1*(" + endOff + ")) ");
-    sql.append(" WHEN NVL(" + table + "is_reversed, 0) = 1 THEN (");
+    sql.append(" WHEN COALESCE(" + table + "is_reversed, 0) = 1 THEN (");
     sql.append(begin.equals(PARAM_VALUE_START) ? "end_max" : "start_min");
     sql.append(" - 1*(" + beginOff + ")) ");
     sql.append("END)");
@@ -482,7 +482,7 @@ public class SpanCompositionPlugin extends AbstractPlugin {
     builder.append("SELECT DISTINCT fl.feature_source_id AS source_id, 'dontcare' as gene_source_id, ");
     builder.append("       fl.sequence_source_id, fl.feature_type, ");
     builder.append("       ca.wdk_weight, ca.project_id, ");
-    builder.append("       NVL(fl.is_reversed, 0) AS is_reversed, ");
+    builder.append("       COALESCE(fl.is_reversed, 0) AS is_reversed, ");
     builder.append("   " + region[0] + " AS begin, " + region[1] + " AS end ");
     builder.append("FROM " + locTable + " fl, " + cacheSql + " ca ");
     builder.append("WHERE fl.feature_source_id = ca.source_id");
@@ -503,7 +503,7 @@ public class SpanCompositionPlugin extends AbstractPlugin {
     builder.append("SELECT DISTINCT ca.source_id, ca.gene_source_id, ");
     builder.append("       fl.sequence_source_id, fl.feature_type, ");
     builder.append("       ca.wdk_weight, ca.project_id, ");
-    builder.append("       NVL(fl.is_reversed, 0) AS is_reversed, ");
+    builder.append("       COALESCE(fl.is_reversed, 0) AS is_reversed, ");
     builder.append("   " + region[0] + " AS begin, " + region[1] + " AS end ");
     builder.append("FROM apidb.FeatureLocation fl, " + cacheSql + " ca ");
     builder.append("WHERE fl.feature_source_id = ca.gene_source_id");
